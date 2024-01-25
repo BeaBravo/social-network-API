cc6c2af20a1723d03f05c224f5a0a07e5a1265dc
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-25 14:16:29 -0700

    created thought routes

--- routes/api/thoughts-route.js
+++ routes/api/thoughts-route.js
@@ -1,3 +1,36 @@
 const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router
+  .route("/")
+  // get all thoughts
+  .get(getThoughts)
+  // create a thought
+  .post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  // get a single thought by id
+  .get(getSingleThought)
+  // update a thought by id
+  .put(updateThought)
+  // delete thought by id
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
