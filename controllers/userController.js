a3efc5c05345abbc07f90d9bfc95183f0cb0950d
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-25 13:56:36 -0700

    started userControllers with helper functions

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const User = require("../models/User");
+
+module.exports = {
+  //get all users
+  async getUsers(req, res) {},
+  // get single user by ID
+  async getSingleUser(req, res) {},
+  // create a new user
+  async createUser(req, res) {},
+  // update user
+  async updateUser(req, res) {},
+  // delete user
+  async deleteUser(req, res) {},
+  // add a new friend
+  async addFriend(req, res) {},
+  // delete friend
+  async deleteFriend(req, res) {},
+};
