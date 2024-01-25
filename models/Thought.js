e1cd0cc45fad4c066403778eb9b88bc9dad437db
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-25 14:37:04 -0700

    created schema for thought model

--- models/Thought.js
+++ models/Thought.js
@@ -2,7 +2,36 @@ const { Schema, model } = require("mongoose");
 const Reaction = require("./Reaction");
 
 // Schema to create a Thought model
-const thoughtSchema = new Schema();
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// create a virtual property "reaction count"
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
 const Thought = model("thought", thoughtSchema);
 module.exports = Thought;
