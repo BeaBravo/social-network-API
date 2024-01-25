813b97525bea50e75fece0357ebc5aebce4b9a16
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-25 14:02:03 -0700

    started thoughtControll with helper functions

---
+++ controllers/thoughtController.js
@@ -0,0 +1,18 @@
+const Thought = require("../models/Thought");
+
+module.exports = {
+  // get all thoughts
+  async getThoughts(req, res) {},
+  // get single thoughts by id
+  async getSingleThought(req, res) {},
+  // create a new thought
+  async createThought(req, res) {},
+  // update a thought
+  async updateThought(req, res) {},
+  // delete a thought
+  async deleteThought(req, res) {},
+  // create a reaction in a single thought
+  async createReaction(req, res) {},
+  // remove a reaction
+  async deleteReaction(req, res) {},
+};
