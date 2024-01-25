add5c7cbe521dc76a769a5ca32613ba15da8b6de
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-25 14:30:13 -0700

    created schema for User model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,43 @@
 const { Schema, model } = require("mongoose");
 
 // Schema to create a User model
-const userSchema = new Schema();
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: "Email invalid",
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//virtual to show the friendCount
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
 const User = model("user", userSchema);
 module.exports = User;
