8f59f3cd75bd1181e4a8a4dee47d3cfbd5206a82
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-25 14:09:02 -0700

    created routes to user route

--- routes/api/users-route.js
+++ routes/api/users-route.js
@@ -1,3 +1,37 @@
 const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController");
 
+// /api/users
+router
+  .route("/")
+  // get all users
+  .get(getUsers)
+  // create a new user
+  .post(createUser);
+
+// /api/users/:userId
+router
+  .route("/:userId")
+  //get single user by userId
+  .get(getSingleUser)
+  //update user by _id
+  .put(updateUser)
+  //delete user by _id
+  .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+  .route("/:userId/friends/:friendId")
+  //add a new friend to a user's friend list
+  .post(addFriend)
+  // delete a friend from user's friend list
+  .delete(deleteFriend);
 module.exports = router;
