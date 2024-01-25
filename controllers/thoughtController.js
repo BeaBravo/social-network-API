70937623512a9153421ed865077c54177a4c9356
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-25 14:55:27 -0700

    tested all routes in insomia core

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -2,17 +2,31 @@ const Thought = require("../models/Thought");
 
 module.exports = {
   // get all thoughts
-  async getThoughts(req, res) {},
+  async getThoughts(req, res) {
+    res.json("will get all thoughts");
+  },
   // get single thoughts by id
-  async getSingleThought(req, res) {},
+  async getSingleThought(req, res) {
+    res.json("will get single thought");
+  },
   // create a new thought
-  async createThought(req, res) {},
+  async createThought(req, res) {
+    res.json("will create a thought");
+  },
   // update a thought
-  async updateThought(req, res) {},
+  async updateThought(req, res) {
+    res.json("will update thought");
+  },
   // delete a thought
-  async deleteThought(req, res) {},
+  async deleteThought(req, res) {
+    res.json("will delete thought");
+  },
   // create a reaction in a single thought
-  async createReaction(req, res) {},
+  async createReaction(req, res) {
+    res.json("will create reaction");
+  },
   // remove a reaction
-  async deleteReaction(req, res) {},
+  async deleteReaction(req, res) {
+    res.json("will delete reaction");
+  },
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,17 +2,31 @@ const User = require("../models/User");
 
 module.exports = {
   //get all users
-  async getUsers(req, res) {},
+  async getUsers(req, res) {
+    res.json("will get all users");
+  },
   // get single user by ID
-  async getSingleUser(req, res) {},
+  async getSingleUser(req, res) {
+    res.json("will get a single user by id");
+  },
   // create a new user
-  async createUser(req, res) {},
+  async createUser(req, res) {
+    res.json("will create user");
+  },
   // update user
-  async updateUser(req, res) {},
+  async updateUser(req, res) {
+    res.json("will update user");
+  },
   // delete user
-  async deleteUser(req, res) {},
+  async deleteUser(req, res) {
+    res.json("will delete user");
+  },
   // add a new friend
-  async addFriend(req, res) {},
+  async addFriend(req, res) {
+    res.json("will add friend");
+  },
   // delete friend
-  async deleteFriend(req, res) {},
+  async deleteFriend(req, res) {
+    res.json("will remove friend");
+  },
 };
