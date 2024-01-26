2f4f520adf5e67b2d3c8375e7b9980596ed99c31
Beatriz Bravo <beatz.bravo@gmail.com>
2024-01-25 17:42:15 -0700

    created seeding files

---
+++ utils/data.js
@@ -0,0 +1,78 @@
+const allUsernames = [
+  "learntino",
+  "harrypotter",
+  "ravenclaw",
+  "maryclaus",
+  "hydrogenlenville",
+  "jacobE",
+  "Summer",
+  "Winter23",
+  "coolusername",
+];
+
+const thoughts = [
+  "This is a cool thought",
+  "What has four wheels and flies? a garbage truck",
+  "Thoughts are the words of our minds",
+  "I have a joke about pizza, but it is too cheesy",
+  "Dogs cannot operate an MRI machine... but catscan",
+  "RIP boiling water, you will be mist",
+];
+
+const reactions = [
+  "this is the best dad joke!",
+  "wow... that was not funny",
+  "LOL!!!!",
+  "I don't get it",
+  "ha.. funny",
+  "I've heard this one before",
+  "Didn't see that coming...",
+  "Get back to work please",
+];
+
+// Get a random item given an array
+const getRandomArrItem = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+// returns the email of the user
+const getEmail = (user) => `${user}@email.com`;
+
+const getRandomUsername = () => getRandomArrItem(allUsernames);
+
+const createReactions = (int) => {
+  if (int === 1) {
+    return {
+      reactionBody: getRandomArrItem(reactions),
+      username: getRandomArrItem(allUsernames),
+    };
+  }
+  results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      reactionBody: getRandomArrItem(reactions),
+      username: getRandomArrItem(allUsernames),
+    });
+  }
+  return results;
+};
+
+const getRandomThoughts = (int) => {
+  const results = [];
+  for (i = 0; i < int; i++) {
+    results.push({
+      thoughtText: getRandomArrItem(thoughts),
+      username: getRandomArrItem(allUsernames),
+      reactions: [...createReactions(2)],
+    });
+  }
+  return results;
+};
+
+const getUsers = () => allUsernames;
+
+module.exports = {
+  getEmail,
+  getRandomThoughts,
+  getRandomUsername,
+  getUsers,
+};
---
+++ utils/seed.js
@@ -0,0 +1,50 @@
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const { getEmail, getRandomThoughts, getUsers } = require("./data");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("connected!");
+  // Delete the collections, if they exist
+  let userCheck = await connection.db
+    .listCollections({ name: "users" })
+    .toArray();
+  if (userCheck.length) {
+    await connection.dropCollection("users");
+  }
+
+  let thoughtCheck = await connection.db
+    .listCollections({ name: "thoughts" })
+    .toArray();
+  if (thoughtCheck.length) {
+    await connection.dropCollection("thoughts");
+  }
+
+  //create seed for thoughts
+  const thoughts = getRandomThoughts(6);
+
+  //create seed for users
+  const allUsers = getUsers();
+  const users = [];
+  for (let i = 0; i < allUsers.length; i++) {
+    const username = allUsers[i];
+    const email = getEmail(username);
+    users.push({
+      username,
+      email,
+    });
+  }
+
+  //insert seeds into collections
+  await User.collection.insertMany(users);
+  await Thought.collection.insertMany(thoughts);
+
+  //console.log to show seeding is done
+  console.table(users);
+  console.table(thoughts);
+  console.log("-------------------seeding complete!-------------------");
+
+  //end the process
+  process.exit(0);
+});
